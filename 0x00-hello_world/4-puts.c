dc6b4c79b14e7c6c9418440b131d644118c4d3c6
Nathaniel <niftynsiah17@gmail.com>
2023-08-10 12:13:17 -0700

    added the script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main- Programming is like building a multilingual puzzle
+ *Return: the return value is zero
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+the readme of the c programming language
