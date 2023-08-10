6ed95349742eed8d0c068aa4e37f6c374012ce02
kingnifty-ctrl <87867472+kingnifty-ctrl@users.noreply.github.com>
2023-08-10 19:32:54 +0000

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ sizeof(a));
 printf("Size of a int: %lu byte(s)\n", (unsigned long)
 sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)
-sizeof(c));
+sizeof(b));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)
 sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)
