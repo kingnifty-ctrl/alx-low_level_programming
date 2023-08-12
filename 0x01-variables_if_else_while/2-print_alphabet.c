5185f60888db9ab893a519c9dd62f381596d8c84
kingnifty-ctrl <87867472+kingnifty-ctrl@users.noreply.github.com>
2023-08-12 05:33:04 +0000

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,7 +13,11 @@ int main(void)
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
-		{putchar(low);
+		{
+		if(low == 'z') 
+			putchar(low);
+			putchar("\n");
+		putchar(low);
 		}
 
 	return (0);
