9067df13ea98ff5727660dceab6f297e75f73385
Nathaniel <niftynsiah17@gmail./com>
2023-08-16 19:14:36 +0000

    solution added

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit- returns last digit
+ * @n: num to return last digit
+ *
+ * Return: returns last digit
+*/
+
+int print_last_digit(int n)
+{
+	int ld;
+
+	ld = n % 10;
+	return (ld);
+}
