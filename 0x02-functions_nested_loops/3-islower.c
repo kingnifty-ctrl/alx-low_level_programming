191c689755c037cd21ea0426cccd630e96d20723
Nathaniel <niftynsiah17@gmail./com>
2023-08-16 18:21:56 +0000

    solution added

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _islower - Confirms lowercase leters
+ *@c : char to confirm its lowercase
+ *
+ * Return : Return 1 on sucess and 0 otherwise
+ */ 
+
+int _islower(int c)
+{
+	if(islower(c))
+	{	return (1);
+	}else {
+		return (0);
+	}
+}
