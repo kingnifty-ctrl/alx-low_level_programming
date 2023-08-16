362c4529916f690f07c05056bbc3bcd89993410f
Nathaniel <niftynsiah17@gmail./com>
2023-08-16 19:06:43 +0000

    solution added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs- returns absolute num.
+ * @n: num to be checked
+ *
+ * Return: returns abs. value
+*/
+
+int _abs(int n)
+{
+	int res;
+
+	res = abs(n);
+	return (res);
+}
