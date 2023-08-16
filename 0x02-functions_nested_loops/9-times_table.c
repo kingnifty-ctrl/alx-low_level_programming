9ff89507a8cfb1f4abce834aa9748acff94eb26e
Nathaniel <niftynsiah17@gmail./com>
2023-08-16 20:20:30 +0000

    solution added

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - print 9 times
+*/
+
+void times_table(void)
+{
+	int i, j, n;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			n = i * j;
+			if (i == 9 && j == 9)
+			{
+				_putchar(n);
+				_putchar('$');
+				_putchar('\n')
+			}
+		}	_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+			 _putchar(' ');
+	}
+}
